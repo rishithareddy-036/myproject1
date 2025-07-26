594f52d5cf98887c5036cdcc94a55912c829ed5a
rishithareddy-036 <krishithareddy21@gmail.com>
2025-07-26 10:21:14 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,16 @@
+class Arithmetic
+{
+public static void main(SAtring args[])
+{
+int n1=5;
+int n2=10;
+int r1=n1+n2;
+int r2=n1-n2;
+int r3=n1*n2;
+int r4=n1/n2;
+System.out.println("Addition:"+r1);
+System.out.println("Subtraction:"+r2);
+System.out.println("Multiplication:"+r3);
+System.out.println("Division:"+r4);
+}
+}
